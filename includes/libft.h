2affeb99633412bddb615184ee6a8f41ba61961c
workmadmax <work.madmax@gmail.com>
2022-12-28 15:34:35 -0300

    building memory folder

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: mdouglas <mdouglas@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 20:11:15 by mdouglas          #+#    #+#             */
-/*   Updated: 2022/12/28 13:12:22 by mdouglas         ###   ########.fr       */
+/*   Updated: 2022/12/28 15:34:07 by mdouglas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,5 +85,22 @@ void    ft_striteri(char *string, void (*f)(unsigned int, char *));
 char    *ft_strjoin(char const *string_one, char const *string_two);
 size_t  ft_strlcat(char *dest, const char *src, size_t size);
 size_t  ft_strlcpy(char *dest, const char *src, size_t size);
+size_t  ft_strlen(const char *string);
+char    *ft_strmapi(char const *string, char (*f)(unsigned int, char));
+int     ft_strncmp(const char *string_one, const char *string_two, size_t n);
+char    *ft_strnstr(const char *big, const char *little, size_t len);
+char    *ft_strrchr(const *string, int c);
+char    *ft_strtrim(char const *string_one, char const *set);
+char    *ft_substr(char const *src, unsigned int start, size_t len);
+
+/* memory */
+
+void    ft_bzero(void *dest, size_t n);
+void    *ft_calloc(size_t count, size_t size);
+void    *ft_memchr(const void *ptr, int c, size_t len);
+int     ft_memcmp(const void *string_one, const void *string_two, size_t n);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
+void    *ft_memmove(void *string_one, const void *string_two, size_t n);
+void    *ft_memset(void *string, int c, size_t n);
 
 #endif
