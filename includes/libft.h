ee3f133fa8bef3ac92e94bd916ac9535ad5a675e
workmadmax <work.madmax@gmail.com>
2022-12-27 21:25:04 -0300

    fix printf

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: mdouglas <mdouglas@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 20:11:15 by mdouglas          #+#    #+#             */
-/*   Updated: 2022/12/27 20:20:33 by mdouglas         ###   ########.fr       */
+/*   Updated: 2022/12/27 21:24:37 by mdouglas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <string.h>
+# include <stdarg.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 999
@@ -36,5 +37,14 @@ char    *get_next_line(int fd);
 char    *get_reading(int fd, char *string);
 char    *get_string(char *string);
 char    *gnl_strjoin(char *string_one, char *string_two);
+
+/* printf */
+
+int     ft_printf(const char *format, ...);
+size_t  ft_parse(va_list args, char format);
+size_t  handle_char(char character);
+size_t  handle_hex(unsigned long x, int prefix, size_t cap);
+size_t  handle_num(long long nbr);
+size_t  handle_str(const char *string);
 
 #endif
